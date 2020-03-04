e7dd1b4e34b0ad70e548b5a0d76bf3e959396230
ernestomelchor <nurturelove88@gmail.com>
2020-03-03 18:30:59 -0600

    created dummy date in seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.new({ name: "Product 1", price: 2.00, image_url: "http://www.example.com/product_1", description: "This product is the most affordable option in our line of products." })
+product.save
+product = Product.new({ name: "Product 2", price: 3.00, image_url: "http://www.example.com/product_2", description: "This product is long-lasting without hurting your pocket." })
+product.save
+product = Product.new({ name: "Product 3", price: 5.00, image_url: "http://www.example.com/product_3", description: "This product is made out of high-quality materials and is a must for all who enjoy our line of products." })
+product.save
+product = Product.new({ name: "Product 4", price: 10.00, image_url: "http://www.example.com/product_4", description: "This product is for those who seek the finest things in life." })
+product.save
+rails db: seed
