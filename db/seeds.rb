324c73e163d5f22231bc0d7c040590f74239ac74
ernestomelchor <nurturelove88@gmail.com>
2020-03-03 18:34:10 -0600

    generated model Product with name, price, image_url, description, and amount

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-product = Product.new({ name: "Product 1", price: 2.00, image_url: "http://www.example.com/product_1", description: "This product is the most affordable option in our line of products." })
+product = Product.new({ name: "Product 1", price: 2.00, image_url: "http://www.example.com/product_1", description: "This product is the most affordable option in our line of products.", amount: 50 })
 product.save
-product = Product.new({ name: "Product 2", price: 3.00, image_url: "http://www.example.com/product_2", description: "This product is long-lasting without hurting your pocket." })
+product = Product.new({ name: "Product 2", price: 3.00, image_url: "http://www.example.com/product_2", description: "This product is long-lasting without hurting your pocket.", amount: 100 })
 product.save
-product = Product.new({ name: "Product 3", price: 5.00, image_url: "http://www.example.com/product_3", description: "This product is made out of high-quality materials and is a must for all who enjoy our line of products." })
+product = Product.new({ name: "Product 3", price: 5.00, image_url: "http://www.example.com/product_3", description: "This product is made out of high-quality materials and is a must for all who enjoy our line of products.", amount: 100 })
 product.save
-product = Product.new({ name: "Product 4", price: 10.00, image_url: "http://www.example.com/product_4", description: "This product is for those who seek the finest things in life." })
+product = Product.new({ name: "Product 4", price: 10.00, image_url: "http://www.example.com/product_4", description: "This product is for those who seek the finest things in life.", amount: 75 })
 product.save
 rails db: seed
