8b6ebab3ce74c13500613e7576c4962c98ebd75d
ernestomelchor <nurturelove88@gmail.com>
2020-03-03 19:11:21 -0600

    succesfully displayed info for first individual product

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,19 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render "all_products.json.jb"
   end
+
+  def first_product_action
+    @products = Product.all
+    @first_product = Product.first
+    render "first_product.json.jb"
+  end
+
+  def second_product_action
+  end
+
+  def third_product_action
+  end
+
+  def fourth_product_action
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/products_url" => "products#products_action"
+    get "/first_product_url" => "products#first_product_action"
+    get "/second_product_url" => "products#second_product_action"
+    get "/third_product_url" => "products#third_product_action"
+    get "/fourth_product_url" => "products#fifth_product_action"
   end
 end
