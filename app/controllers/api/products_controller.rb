4f9bd7ed46d627f51eeb920c6981f9ac86e8372d
ernestomelchor <nurturelove88@gmail.com>
2020-03-11 19:50:14 -0500

    Add new model Supplier to database

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,29 @@
 class Api::ProductsController < ApplicationController
   def index
-    @products = Product.all.order("id")
+    @products = Product.all
+
+    if params[:search]
+      @products = @products.where("name ILIKE ?", "%#{params[:search]}%")
+    end
+
+    if params[:discount]
+      @products = @products.where("price < 250")
+    end
+
+    # These lines will work specifically for price
+    # if params[:sort] == "price" && params[:sort_order] == "asc"
+    #   @products = @products.order(price: :asc)
+    # elsif params[:sort] == "price" && params[:sort_order] == "desc"
+    #   @products = @products.order(price: :desc)
+
+    # This line of code is more dynamic and will work for other parameters so you can create links in your front-end to help sort
+    if params[:sort] && params[:sort_order]
+      @products = @products.order(params[:sort] => params[:sort_order])
+    else
+      @products = @products.order(id: :asc)
+    end
+
+    @products = @products.order(id: :asc)
     render "index.json.jb"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# products
 product = Product.new({ name: "Product 1", price: 2.00, image_url: "http://www.example.com/product_1", description: "This product is the most affordable option in our line of products.", amount: 50 })
 product.save
 product = Product.new({ name: "Product 2", price: 3.00, image_url: "http://www.example.com/product_2", description: "This product is long-lasting without hurting your pocket.", amount: 100 })
@@ -14,3 +15,11 @@ product = Product.new({ name: "Product 3", price: 5.00, image_url: "http://www.e
 product.save
 product = Product.new({ name: "Product 4", price: 10.00, image_url: "http://www.example.com/product_4", description: "This product is for those who seek the finest things in life.", amount: 75 })
 product.save
+
+# suppliers
+supplier = Supplier.new({ name: "Nike", email: "nike@example.com", phone_number: "777-777-7777" })
+supplier.save
+supplier = Supplier.new({ name: "Adidas", email: "adidas@example.com", phone_number: "888-888-8888" })
+supplier.save
+supplier = Supplier.new({ name: "Puma", email: "puma@example.com", phone_number: "999-999-9999" })
+supplier.save
