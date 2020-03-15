c4a0b9fbd04965daf321e693901525c598fb6f5f
ernestomelchor <nurturelove88@gmail.com>
2020-03-15 11:58:48 -0500

    add user_id to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ApplicationRecord
 
   belongs_to :supplier # This line takes care of lines 8-10 so long as you have designed your database this way
   has_many :images
+  belongs_to :user
 
   # def supplier
   #   Supplier.find_by(id: supplier_id)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_15_153748) do
+ActiveRecord::Schema.define(version: 2020_03_15_165737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_03_15_153748) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "supplier_id"
+    t.integer "user_id"
   end
 
   create_table "suppliers", force: :cascade do |t|
