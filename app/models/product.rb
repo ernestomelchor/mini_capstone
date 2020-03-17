ef74c7e832e6a182165cbcc3635f3873c5c63bb7
ernestomelchor <nurturelove88@gmail.com>
2020-03-16 22:35:51 -0500

    add orders action using association methods in Order model

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,12 +4,13 @@ class Product < ApplicationRecord
   validates :description, presence: true, length: { minimum: 10, maximum: 500 }
 
   belongs_to :supplier # This line takes care of lines 8-10 so long as you have designed your database this way
-  has_many :images
-  belongs_to :user
 
   # def supplier
   #   Supplier.find_by(id: supplier_id)
   # end
+
+  has_many :images
+  has_many :orders
 
   def discount
     price < 250
