68f7ddd93de2edc11393139bc87fc4b9bf96d46f
ernestomelchor <nurturelove88@gmail.com>
2020-03-25 18:23:40 -0500

    add code to allow autocomplete on erb files

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,25 @@ class ProductsController < ApplicationController
     @products = Product.all
     render "index.html.erb"
   end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render "show.html.erb"
+  end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      amount: params[:amount],
+      supplier_id: params[:supplier_id],
+    )
+    @product.save
+    redirect_to "/products/#{@product.id}"
+  end
 end
