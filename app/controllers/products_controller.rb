91802aa7637edad9b007ee18d13df100fd3a2a1a
ernestomelchor <nurturelove88@gmail.com>
2020-03-25 20:19:26 -0500

    created frontend with actions and views and redirects

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,4 +24,29 @@ class ProductsController < ApplicationController
     @product.save
     redirect_to "/products/#{@product.id}"
   end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+    render "edit.html.erb"
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name] || @product.name # If the previous condition is falsey, move forward, meaning keep the previous @product.name the same
+    @product.price = params[:price] || @product.price
+    @product.description = params[:description] || @product.description
+    @product.amount = params[:amount] || @product.amount
+    @product.supplier_id = params[:supplier_id] || @product.supplier_id
+    if @product.save
+      redirect_to "/products/#{@product.id}"
+    else
+      render "edit.html.erb"
+    end
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.destroy
+    redirect_to "/products"
+  end
 end
